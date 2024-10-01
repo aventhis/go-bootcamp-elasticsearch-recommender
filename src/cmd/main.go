af00e19ef26b156cc5ae2c6eb12e59d96bec163d
jenisepl <jenisepl@student.21-school.ru>
2024-10-01 15:03:03 +0300

    add code for create  elasticsearch.NewDefaultClient()

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,1 +1,28 @@
-package cmd
+package main
+
+import (
+	"fmt"
+	"github.com/elastic/go-elasticsearch/v8"
+	"log"
+)
+
+func main() {
+	es, err := elasticsearch.NewDefaultClient()
+	if err != nil {
+		log.Fatalf("Error creating the client: %s\n", err)
+	}
+
+	res, err := es.Info()
+	if err != nil {
+		log.Fatalf("Error getting the response: %s\n", err)
+	}
+
+	defer res.Body.Close()
+
+	if res.IsError() {
+		log.Fatalf("Error response from Elasticsearch: %s\n", res.String())
+	}
+
+	// Выводим статус ответа
+	fmt.Printf("Elasticsearch response status: %s\n", res.Status())
+}
