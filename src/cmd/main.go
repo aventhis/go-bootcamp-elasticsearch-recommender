9455459d39d34a6cb78effc2d3158331e423f125
jenisepl <jenisepl@student.21-school.ru>
2024-10-01 15:19:34 +0300

    add  es.Indices.Create

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"github.com/elastic/go-elasticsearch/v8"
 	"log"
+	"strings"
 )
 
 func main() {
@@ -12,17 +12,44 @@ func main() {
 		log.Fatalf("Error creating the client: %s\n", err)
 	}
 
-	res, err := es.Info()
-	if err != nil {
-		log.Fatalf("Error getting the response: %s\n", err)
-	}
+	//res, err := es.Info()
+	//if err != nil {
+	//	log.Fatalf("Error getting the response: %s\n", err)
+	//}
+	//
+	//defer res.Body.Close()
+	//
+	//if res.IsError() {
+	//	log.Fatalf("Error response from Elasticsearch: %s\n", res.String())
+	//}
+	//
+	//// Выводим статус ответа
+	//fmt.Printf("Elasticsearch response status: %s\n", res.Status())
 
+	mapping := `{
+		"mappings" {
+			"properties": {
+				"name": {
+					"type":  "text"
+				},
+				"address": {
+					"type":  "text"
+				},
+				"phone": {
+					"type":  "text"
+				},
+				"location": {
+				  "type": "geo_point"
+				}
+  			}
+		}
+	}`
+
+	res, err := es.Indices.Create("places", es.Indices.Create.WithBody(strings.NewReader(mapping)))
+	if err != nil {
+		log.Fatalf("Ошибка при создании индекса: %s", err)
+	}
 	defer res.Body.Close()
 
-	if res.IsError() {
-		log.Fatalf("Error response from Elasticsearch: %s\n", res.String())
-	}
-
-	// Выводим статус ответа
-	fmt.Printf("Elasticsearch response status: %s\n", res.Status())
+	log.Println("Индекс и маппинг созданы успешно")
 }
