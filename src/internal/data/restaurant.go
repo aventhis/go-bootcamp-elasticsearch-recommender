37d434bddc274770c433a03222db6120bb292d00
jenisepl <jenisepl@student.21-school.ru>
2024-10-02 16:11:31 +0300

    add struct Restaurant and data

---
+++ src/internal/data/restaurant.go
@@ -0,0 +1,12 @@
+package data
+
+type Restaurant struct {
+	Id       string `json:"id"`
+	Name     string `json:"name"`
+	Address  string `json:"address"`
+	Phone    string `json:"phone"`
+	Location struct {
+		Lat float64 `json:"lat"`
+		Lon float64 `json:"lon"`
+	} `json:"location"`
+}
