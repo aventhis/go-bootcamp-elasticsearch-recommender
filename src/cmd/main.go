732c4b5c5f95ba73a3b9484902992fff95f44e1f
jenisepl <jenisepl@student.21-school.ru>
2024-10-02 15:55:25 +0300

    add package elasticsearch

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,55 +1,22 @@
 package main
 
 import (
+	"fmt"
+	elasticsearch_ "github.com/aventhis/go-bootcamp-elasticsearch-recommender/src/internal/elasticsearch"
 	"github.com/elastic/go-elasticsearch/v8"
 	"log"
-	"strings"
 )
 
 func main() {
 	es, err := elasticsearch.NewDefaultClient()
 	if err != nil {
-		log.Fatalf("Error creating the client: %s\n", err)
+		log.Fatalf("Ошибка при создании клиента Elasticsearch: %s\n", err)
 	}
 
-	//res, err := es.Info()
-	//if err != nil {
-	//	log.Fatalf("Error getting the response: %s\n", err)
-	//}
-	//
-	//defer res.Body.Close()
-	//
-	//if res.IsError() {
-	//	log.Fatalf("Error response from Elasticsearch: %s\n", res.String())
-	//}
-	//
-	//// Выводим статус ответа
-	//fmt.Printf("Elasticsearch response status: %s\n", res.Status())
-
-	mapping := `{
-		"mappings" {
-			"properties": {
-				"name": {
-					"type":  "text"
-				},
-				"address": {
-					"type":  "text"
-				},
-				"phone": {
-					"type":  "text"
-				},
-				"location": {
-				  "type": "geo_point"
-				}
-  			}
-		}
-	}`
-
-	res, err := es.Indices.Create("places", es.Indices.Create.WithBody(strings.NewReader(mapping)))
+	err = elasticsearch_.CreateIndex(es)
 	if err != nil {
-		log.Fatalf("Ошибка при создании индекса: %s", err)
+		log.Fatalf("Ошибка при создании индекса: %s\n", err)
 	}
-	defer res.Body.Close()
 
-	log.Println("Индекс и маппинг созданы успешно")
+	fmt.Println("the end")
 }
---
+++ src/internal/elasticsearch/index.go
@@ -0,0 +1,56 @@
+package elasticsearch_
+
+import (
+	"fmt"
+	"github.com/elastic/go-elasticsearch/v8"
+	"log"
+	"strings"
+)
+
+func CreateIndex(es *elasticsearch.Client) error {
+	mapping := `{
+		"mappings": {
+			"properties": {
+				"id": {
+					"type": "long"
+				},
+				"name": {
+					"type":  "text"
+				},
+				"address": {
+					"type":  "text"
+				},
+				"phone": {
+					"type":  "text"
+				},
+				"location": {
+					"type": "geo_point"
+				}
+  			}
+		}
+	}`
+
+	// Удаляем индекс, если он уже существует
+	res, err := es.Indices.Delete([]string{"places"}, es.Indices.Delete.WithIgnoreUnavailable(true))
+	if err != nil {
+		return fmt.Errorf("ошибка при удалении индекса: %s", err)
+	}
+	defer res.Body.Close()
+
+	// Создаем индекс с маппингом
+	res, err = es.Indices.Create(
+		"places",
+		es.Indices.Create.WithBody(strings.NewReader(mapping)),
+	)
+	if err != nil {
+		return fmt.Errorf("ошибка при создании индекса: %s", err)
+	}
+	defer res.Body.Close()
+
+	if res.IsError() {
+		return fmt.Errorf("ошибка ответа от Elasticsearch: %s", err)
+	}
+
+	log.Println("Индекс 'places' и маппинг созданы успешно")
+	return nil
+}
