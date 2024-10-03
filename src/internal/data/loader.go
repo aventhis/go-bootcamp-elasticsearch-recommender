4883c92519bee1f7f8938fe144c32f3c6c134793
jenisepl <jenisepl@student.21-school.ru>
2024-10-03 12:40:31 +0300

    change ID +1

--- src/internal/data/loader.go
+++ src/internal/data/loader.go
@@ -35,11 +35,11 @@ func LoadRestaurant(filePath string) ([]Restaurant, error) {
 			continue
 		}
 		id, _ := strconv.ParseInt(record[0], 10, 64)
-		lat, _ := strconv.ParseFloat(record[4], 64)
-		lon, _ := strconv.ParseFloat(record[5], 64)
+		lat, _ := strconv.ParseFloat(record[5], 64)
+		lon, _ := strconv.ParseFloat(record[4], 64)
 
 		restaurant := Restaurant{
-			ID:      id,
+			ID:      id + 1,
 			Name:    record[1],
 			Address: record[2],
 			Phone:   record[3],
